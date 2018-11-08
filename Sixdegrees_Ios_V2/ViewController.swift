c6a90be0f0f6c8f3c008af7c86c75c353ab20d79
CaryTsai1015 <36913550+CaryTsai1015@users.noreply.github.com>
2018-11-09 01:31:59 +0800

    Initial Commit

---
+++ Sixdegrees_Ios_V2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sixdegrees_Ios_V2
+//
+//  Created by Tsai Cary on 2018/11/9.
+//  Copyright © 2018 Tsai Cary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
